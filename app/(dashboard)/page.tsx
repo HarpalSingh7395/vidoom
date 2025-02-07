7237ae5d41c38766cfd0a0521182a77d377499cc
HarpalSingh7395 <harrysinghharpal58@gmail.com>
2025-02-07 12:09:05 -0500

    Updated dashboard design.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,29 @@
+import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 
 
 export default function Dashboard() {
+  const now = new Date();
+
+  const time = now.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit'
+  });
+  const date = (new Intl.DateTimeFormat('en-US', {
+    dateStyle: "full"
+  })).format(now);
   return (
     <div className="flex flex-col gap-10">
       <h2 className="text-4xl font-bold">Dashboard</h2>
-      <div className="relative w-full overflow-hidden rounded-lg h-64">
-        <Image  src={"/dashboard.avif"} alt="dashboard-image" fill style={{objectFit: "cover"}} />
-          
+      <div className="relative w-full overflow-hidden rounded-xl h-64 bg-card p-6 flex flex-col justify-between">
+        {/* <Image  src={"/dashboard.avif"} alt="dashboard-image" fill style={{objectFit: "cover"}} /> */}
+        <div>
+          <Badge>Upcoming Meeting: Feb 7, 2024 at 10:00 AM</Badge>
+        </div>
+        <div>
+          <p className="text-5xl font-bold">{time}</p>
+          <p className="text-muted-foreground">{date}</p>
+        </div>
       </div>
     </div>
   )
