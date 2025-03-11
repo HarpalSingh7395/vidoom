1f4a6883a3c29538205300f6340c630d3abc070b
HarpalSingh7395 <harrysinghharpal58@gmail.com>
2025-03-11 12:30:57 +0530

    Added logo image.

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -9,7 +9,7 @@ import {
 import { Menu } from "lucide-react"
 import NavItem from "./sidebar/SidebarItem"
 import { navLinks } from "./sidebar/Sidebar"
-import { Brand } from "./icons"
+import Image from "next/image"
 
 export default function MobileNavbar() {
     return (
@@ -19,8 +19,10 @@ export default function MobileNavbar() {
             </SheetTrigger>
             <SheetContent side={'left'} className="px-4 w-full max-w-[264px]">
                 <section className=''>
-                    <SheetHeader className="mb-3">
-                        <SheetTitle><Brand className='w-10 h-10 fill-primary' /></SheetTitle>
+                    <SheetHeader className="mb-8">
+                        <SheetTitle>
+                            <Image src={"/logo.webp"} alt="Vidoom" height={40} width={40} />
+                            </SheetTitle>
                     </SheetHeader>
                     {navLinks.map(item => (<SheetClose asChild key={item.name}>
                         <NavItem item={item} />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Brand } from './icons'
 import {
     SignInButton,
     SignedIn,
@@ -7,12 +6,13 @@ import {
     UserButton,
 } from '@clerk/nextjs'
 import MobileNavbar from './MobileNavbar'
+import Image from 'next/image'
 
 
 export default function Navbar() {
     return (
         <div className='px-8 max-lg:px-6 py-4 flex justify-between items-center bg-card fixed top-0 left-0 w-full z-10'>
-            <Brand className='w-10 h-10 fill-primary' />
+            <Image src={"/logo.webp"} alt="Vidoom" height={40} width={40} />
             <SignedOut>
                 <SignInButton />
             </SignedOut>
